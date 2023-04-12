ba96f4e7b7f9c1ff0e3b9bd2a4f73d6d8673e8bc
arthur-ngdi <arthur,f@gec.inatel.br>
2023-04-12 14:59:56 -0300

    aula 2 do capítulo 4

--- aula_cap4/matriz.py
+++ aula_cap4/matriz.py
@@ -6,8 +6,13 @@ mtz = np.array([
     [7,8,9]
 ])
 
-print(mtz/10)
-print(mtz*20)
+# print(mtz/10)
+# print(mtz*20)
 
-print(mtz.mean(axis=0)[1]) #axis=0 => colunas
-print(mtz.sum(axis=1)[0]) #axis=1 => linhas
+# print(mtz.mean(axis=0)[1]) #axis=0 => colunas
+# print(mtz.sum(axis=1)[0]) #axis=1 => linhas
+
+#CONDICIONAIS DO NUMPY
+
+even_mtz = mtz[mtz%2==1]
+print(even_mtz)
