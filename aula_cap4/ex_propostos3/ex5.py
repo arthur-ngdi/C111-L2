b9eab1cd3788ab5acc37404ed5bca6c6f2af7585
arthur-ngdi <arthur.f@gec.inatel.br>
2023-04-27 20:23:30 -0300

    correção no código

--- aula_cap4/ex_propostos3/ex5.py
+++ aula_cap4/ex_propostos3/ex5.py
@@ -5,8 +5,8 @@ arr = np.loadtxt('aula_cap4/files/space.csv',
                  encoding='utf-8', 
                  dtype=str)
 
-empresas = arr[0:,1]
+#empresas = arr[0:,1]
 
-empresa, quant_missoes = np.unique(empresas, return_counts = True)
+empresa, quant_missoes = np.unique(arr[0:,1], return_counts = True)
 
 print(np.stack((empresa,quant_missoes),axis=1)) #a função stack selecionando as colunas (axis=1) junta os dois arrays em uma matriz(x,2)
